89dc681479110c5c9a3ceeb53f2ef745de14dfb2
An Hoang <ansofiathefirst@gmail.com>
2023-06-24 19:28:37 -0400

    Adding db to Firebase database storage

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,17 +1,18 @@
-import {useContext} from 'react';
-import { ProductsContext } from '../../contexts/product.context';
-import ProductCard from '../../components/product-card/product-card.component';
+import Category from '../category/category.component';
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import { Routes, Route } from'react-router-dom';
+
 import './shop.styles.scss';
 
 const Shop = () => {
-    const {products} = useContext(ProductsContext);
-    return (
-        <div className='products-container'>
-            {products.map((product) => (
-                <ProductCard key={product.id} product={product}/>
-    ))}
-        </div>
-    )
-}
+
+return(
+  <Routes>
+      <Route index element={<CategoriesPreview/>} />
+      <Route path=":category" element={<Category/>} />
+
+  </Routes>
+)}
+  
 
 export default Shop;
